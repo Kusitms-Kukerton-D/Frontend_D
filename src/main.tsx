9f9907cd47961fcc39585785ed0b8b641b562507
parkyejin <uiop5809@naver.com>
2024-04-04 14:15:38 +0900

    init: 초기 세팅

---
+++ src/main.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Router from "./Router.tsx";
+import { RecoilRoot } from "recoil";
+import { theme } from "./styles/theme";
+import { ThemeProvider } from "styled-components";
+import GlobalFont from "./styles/GlobalFont.ts";
+import GlobalStyle from "./styles/GlobalStyle.ts";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <RecoilRoot>
+      <ThemeProvider theme={theme}>
+        <GlobalFont />
+        <GlobalStyle />
+        <Router />
+      </ThemeProvider>
+    </RecoilRoot>
+  </React.StrictMode>
+);
