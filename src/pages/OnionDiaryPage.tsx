b7c0ee7ee83ee2b4cee13ff593b7284d88a2e8f8
parkyejin <uiop5809@naver.com>
2024-04-07 04:31:29 +0900

    feat: 양파 키우기 페이지

---
+++ src/pages/OnionDiaryPage.tsx
@@ -0,0 +1,32 @@
+import { styled } from "styled-components";
+import Navigation from "../components/navigation/Navigation";
+
+const OnionDiaryPage = () => {
+  return (
+    <Layout>
+      <Content>1</Content>
+      <NavigationLayout>
+        <Navigation />
+      </NavigationLayout>
+    </Layout>
+  );
+};
+
+export default OnionDiaryPage;
+
+const Layout = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+`;
+
+const Content = styled.div`
+  flex: 1;
+`;
+
+const NavigationLayout = styled.div`
+  position: fixed;
+  bottom: 0;
+  width: 390px;
+  z-index: 1;
+`;
