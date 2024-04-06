3a72d5fa3d41bf3757414263a9fb1b8f89b8dc67
aaminha <dks4857@gmail.com>
2024-04-06 21:38:37 +0900

    design: 폰 헤더 구현

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MainPage from "./pages/MainPage";
+import InformationPage from "./pages/InformationPage";
+import Layout from "./components/Layout";
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<MainPage />} />
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/information" element={<InformationPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
